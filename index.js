edac12707701a11b9418844b83bc28f90cc31f77
wantaman <141419563+wantaman@users.noreply.github.com>
2023-09-30 22:45:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("MOCK_UP.json");
+const router = jsonServer.router("MOCK_DATA.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
